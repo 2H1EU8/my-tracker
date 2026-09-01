2cf7698333ae5c758242a39bd3b1c114922ce281
2H1EU8 <dhieu03@gmail.com>
2026-09-01 15:48:34 +0700

    chore: remove scratch python scripts from main

--- patch-domain-errors.py
+++
@@ -1,7 +0,0 @@
-with open("src/domain/errors.ts", "r") as f:
-    content = f.read()
-
-content = content.replace('"invalid_title"', '"invalid_title"\n  | "invalid_ai_plan"')
-
-with open("src/domain/errors.ts", "w") as f:
-    f.write(content)
